09b9d0e1a67d968240fec40b392d785fb66b595a
agst552 <augustofrank4@gmail.com>
2025-07-10 20:21:25 -0300

    Criação do dropMenu.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,6 @@
 import initTab from "./modules/tabNavigation.js";
 import initAccordion from "./modules/Accordion.js";
+import initDropMenu from "./modules/dropMenu.js";
 initTab();
+initDropMenu();
 initAccordion();
